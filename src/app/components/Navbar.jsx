0127b37e5f1d252f0a89ca1567f0090726898296
Asif Ahmed <asifahmedakash32@gmail.com>
2026-04-14 19:55:59 +0600

    added banner and navabar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,25 +1,62 @@
 // import React from 'react';
+'use client'
+import { usePathname } from "next/navigation";
+import { FaHome, FaClock, FaChartBar } from "react-icons/fa";
+import Link from "next/link";
 
 const Navbar = () => {
+    const pathname = usePathname();
+
+    const navLinks = [
+        {
+            name: "Home",
+            path: "/home",
+            icon: FaHome,
+        },
+        {
+            name: "Timeline",
+            path: "/timeline",
+            icon: FaClock,
+        },
+        {
+            name: "States",
+            path: "/stats",
+            icon: FaChartBar,
+        },
+    ];
+
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className=" bg-base-100 shadow-sm ">
+            <div className="navbar max-w-360 mx-auto">
+                <div className="flex-1">
+
+                    <Link href="/home" className="btn btn-ghost text-[24px]">
+                        Keen<span className="font-normal">Keeper</span>
+                    </Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        {navLinks.map((link, index) => {
+                            const Icon = link.icon;
+                            const isActive = pathname === link.path;
+                            return (
+                                <li className={`btn ${!isActive ? "btn-ghost" : "btn-success"}`} key={index}>
+                                    <Link
+                                        href={link.path}
+                                        className={`flex items-center gap-2 px-2 py-1 rounded-md ${isActive ? "text-white" : "text-[#64748B]"
+                                            }`}
+                                    >
+                                        <Icon />
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+
+                    </ul>
+                </div>
             </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
-            </div>
+
         </div>
     );
 };
--- src/app/home/componets/Banner.jsx
+++ src/app/home/componets/Banner.jsx
@@ -1,18 +1,53 @@
 // import React from 'react';
+import { FaPlus } from "react-icons/fa";
+
 
 const Banner = () => {
+
+    const stats = [
+        {
+            title: "Total Friends",
+            value: 10,
+        },
+        {
+            title: "On Track",
+            value: 3,
+        },
+        {
+            title: "Need Attention",
+            value: 4,
+        },
+        {
+            title: "Interactions This Month",
+            value: 12,
+        },
+    ];
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Hello there</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                    <button className="btn btn-primary">Get Started</button>
+            <div className="text-center">
+                <div className="hero-content">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Friends to keep close in your life</h1>
+                        <p className="py-6">
+                            Your personal shelf of meaningful connections. Browse, tend, and nurture the
+                            relationships that matter most.
+                        </p>
+                        <button className="btn btn-success"><FaPlus />Add a Friend</button>
+                    </div>
+                </div>
+                <div className="stats shadow">
+                    <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                        {stats.map((item, index) => (
+                            <div key={index} className="stat place-items-center bg-base-200 rounded-lg">
+
+                                <div className="stat-value">{item.value}</div>
+                                <div className="stat-title">{item.title}</div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
+
         </div>
     );
 };
