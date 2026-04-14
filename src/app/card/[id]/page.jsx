981d1f6d9413c94cb9b7e88bac4462ffc08312bb
Asif Ahmed <asifahmedakash32@gmail.com>
2026-04-14 18:05:00 +0600

    folder structure with basic tests done

---
+++ src/app/card/[id]/page.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const CardDetailsPage = () => {
+    return (
+        <div>
+            this is card details
+        </div>
+    );
+};
+
+export default CardDetailsPage;
---
+++ src/app/home/page.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+import Banner from "./componets/Banner";
+import KeenCard from "./componets/KeenCard";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            {/* <KeenCard></KeenCard> */}
+        </div>
+    );
+};
+
+export default HomePage;
